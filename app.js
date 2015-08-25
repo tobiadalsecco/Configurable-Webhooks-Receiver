b4e0a8623b35698098e180051257e07b260bb818
tobiadalsecco <tobia.dalsecco@gmail.com>
2015-08-25 11:04:08 -0300

    npm module

--- app.js
+++ app.js
@@ -6,52 +6,68 @@
 		This is the server configuration.
 */
 
-var express = require('express'),
-	http = require('http'),
-	config = require('./config'),
-	app = express();
+module.exports = function(config){
 
-// all environments
-app.set('port', process.env.PORT || config.port);
+	var cwhr = function(){};
 
-app.get(config.healtUrl, function (req, res, next) {
-	res.send('WebHook Receiver is working');
-});
+	var express = require('express'),
+		http = require('http'),
+		config = require('./config'),
+		app = express(),
+		isDeploying = false;
 
-var baseUrl = config.deploysBaseUrl;
+	// all environments
+	app.set('port', process.env.PORT || config.port);
 
-for(i in config.deploys){
-
-	var depl = config.deploys[i];
-	
-	app.post(baseUrl + depl.url, function (req, res) {
-
-	 	var spawn = require('child_process').spawn,
-	     deploy = spawn('sh', [ './deploy.sh', depl.dir, depl.branch ]);
-
-    deploy.stdout.on('data', function (data) {
-
-	  	console.log(''+data);
-
-	  	var afterDeploy = spawn('sh', [ depl.afterDeploy.script ]);
-	  	afterDeploy.on('data', function (_data) {
-        console.log(''+_data);
-    	});
-	  	afterDeploy.on('close', function (code) {
-        console.log('Child process exited with code ' + code);
-    	});
-
-		});
-
-    deploy.on('close', function (code) {
-        console.log('Child process exited with code ' + code);
-    });
-
-    res.json(200, {message: 'WebHook received!'})
+	app.get(config.healtUrl, function (req, res, next) {
+		if(isDeploying){
+			res.send('WebHook Receiver is deploying...');
+		} else {
+			res.send('WebHook Receiver is waiting...');
+		}
 	});
 
-}
+	var baseUrl = config.deploysBaseUrl;
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+	for(i in config.deploys){
+
+		var depl = config.deploys[i];
+
+		app.post(baseUrl + depl.url, function (req, res) {
+
+			isDeploying = true;
+
+		 	var spawn = require('child_process').spawn,
+		     deploy = spawn('sh', [ './deploy.sh', depl.dir, depl.branch ]);
+
+	    deploy.stdout.on('data', function (data) {
+
+		  	console.log(''+data);
+
+		  	var afterDeploy = spawn('sh', [ depl.afterDeploy.script ]);
+		  	afterDeploy.on('data', function (_data) {
+	        console.log(''+_data);
+	    	});
+		  	afterDeploy.on('close', function (code) {
+	        console.log('Child process exited with code ' + code);
+	        isDeploying = false;
+	    	});
+
+			});
+
+	    deploy.on('close', function (code) {
+	        console.log('Child process exited with code ' + code);
+	    });
+
+	    res.json(200, {message: 'WebHook received. Deploy triggered'});
+	    
+		});
+
+	}
+
+	http.createServer(app).listen(app.get('port'), function(){
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
+
+	return cwhr;
+}
